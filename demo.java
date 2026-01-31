5fb356a40165195e5f8bb70af4cb3e0e8961f4a8
chandu121-ui <chanduchirra702@gmail.com>
2026-01-31 15:25:12 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class demo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name);
+
+        sc.close();
     }
 }
