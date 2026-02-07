b3597e44e8b01905bf2cb3ac55d460dee4648d7e
chandu121-ui <chanduchirra702@gmail.com>
2026-02-07 00:41:06 -0800

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Modulus: " + remainder);
+    }
+}
